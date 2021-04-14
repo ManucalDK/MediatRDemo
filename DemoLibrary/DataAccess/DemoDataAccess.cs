db6649b2628168ea878f2ce5357c5ccb6a95f714
manuel.calambas <manuel.calambas@ceiba.com.co>
2021-04-14 17:53:57 -0500

    Add project files.

---
+++ DemoLibrary/DataAccess/DemoDataAccess.cs
@@ -0,0 +1,31 @@
+﻿using DemoLibrary.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DemoLibrary.DataAccess
+{
+    public class DemoDataAccess : IDataAccess
+    {
+        private List<PersonModel> people = new();
+
+        public DemoDataAccess()
+        {
+            people.Add(new PersonModel { Id = 1, FirstName = "Manuel", LastName = "Calambas" });
+            people.Add(new PersonModel { Id = 2, FirstName = "Martha", LastName = "Giraldo" });
+            people.Add(new PersonModel { Id = 3, FirstName = "Kiara", LastName = "Maria" });
+        }
+
+        public List<PersonModel> GetPeople()
+        {
+            return people;
+        }
+
+        public PersonModel InsertPerson(string firstName, string lastName)
+        {
+            PersonModel p = new PersonModel() { FirstName = firstName, LastName = lastName };
+            p.Id = people.Max(x => x.Id) + 1;
+            people.Add(p);
+            return p;
+        }
+    }
+}
